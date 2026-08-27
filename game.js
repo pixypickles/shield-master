405f40f5d74218f0d2bab7de480dbdc8fd6974b7
pixypickles <pirotototo@gmail.com>
2026-08-28 07:01:14 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -25,7 +25,7 @@ const weapons=[
 ];
 const player={x:230,y:545,r:28,speed:230,hp:100,maxHp:100,fallGrace:0,face:'down',aim:0,shield:false,jumpT:0,jumpDur:.62,jumpHeight:105,attacking:0,spin:0,spinT:0,attackMax:.22,attackCooldown:0,charging:false,chargeStart:0,skillT:0,skillElapsed:0,skillBase:0,skillSide:1,skillHit:new Set(),skillKind:'',skillPhase:0,skillZ:0,hammerSpin:0,fireTrail:[],iceTrail:[],spiral:0,spiralA:0,hammerSmash:0,hammerSmashT:0,weapon:0,shieldType:0,inv:0,walkPhase:0,moveMag:0,dashT:0,dashAuto:false,dashDir:0,dashAttack:false,dashShieldHit:new Set(),shieldStepT:0,shieldStepDir:0,airAttack:false,airAttackDone:false,airMagic:null,airSlam:false};
 
-// Prototype 56: 最初の浮遊草原ステージ
+// Prototype 57: 最初の浮遊草原ステージ
 const stage={
  id:1,
  bossDefeated:false,
@@ -2258,7 +2258,8 @@ function drawActiveWeapon(wx,wy,wa,thrust){
    circle(0,0,7,'#f7fbff','#111',4);
    ctx.restore();
  }else{
-   drawActiveWeapon(wx,wy,wa,thrust);
+   // 通常時は元の武器描画へ戻す。ここで自分自身を呼ぶと無限再帰してゲーム全体が停止する。
+   drawWeapon(wx,wy,wa,thrust);
  }
 }
 
